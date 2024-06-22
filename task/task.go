dcf59fdd56832877685f545cda596c3a0d010001
guths <jonathanguths@gmail.com>
2024-06-22 12:04:11 -0300

    creating worker transations

--- task/task.go
+++ task/task.go
@@ -20,11 +20,33 @@ type State int
 
 const (
 	Pending State = iota
-	Schedule
+	Scheduled
 	Running
 	Completed
 	Failed
 )
+
+var stateTransitionMap = map[State][]State{
+	Pending:   {Scheduled},
+	Scheduled: {Scheduled, Running, Failed},
+	Running:   {Running, Completed, Failed},
+	Completed: {},
+	Failed:    {},
+}
+
+func Contains(states []State, state State) bool {
+	for _, s := range states {
+		if s == state {
+			return true
+		}
+	}
+
+	return false
+}
+
+func ValidStateTranstion(src State, dst State) bool {
+	return Contains(stateTransitionMap[src], dst)
+}
 
 type Task struct {
 	ID            uuid.UUID
--- worker/worker.go
+++ worker/worker.go
@@ -2,6 +2,8 @@ package worker
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -15,6 +17,10 @@ type Worker struct {
 	TaskCount int
 }
 
+func (w *Worker) AddTask(t *task.Task) {
+	w.Queue.Enqueue(t)
+}
+
 func (w *Worker) CollectStats() {
 	fmt.Println("I will collect stats")
 }
@@ -23,8 +29,27 @@ func (w *Worker) RunTask() {
 	fmt.Println("I will start or stop a task")
 }
 
-func (w *Worker) StartTask() {
-	fmt.Println("I will start a task")
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
+	t.StartTime = time.Now().UTC()
+
+	config := task.NewConfig(&t)
+
+	d := task.NewDocker(config)
+
+	result := d.Run()
+
+	if result.Error != nil {
+		log.Printf("Err running task %v: %v\n", t.ID, result.Error)
+		t.State = task.Failed
+		w.Db[t.ID] = &t
+		return result
+	}
+
+	t.ContainerID = result.ContainerId
+	t.State = task.Running
+	w.Db[t.ID] = &t
+
+	return result
 }
 
 func (w *Worker) StopTask(t task.Task) {
@@ -32,5 +57,17 @@ func (w *Worker) StopTask(t task.Task) {
 
 	d := task.NewDocker(config)
 
-	d.Stop(t.)
+	result := d.Stop(t.ContainerID)
+
+	if result.Error != nil {
+		log.Printf("Error stopping container %v: %v\n", t.ContainerID, result.Error)
+	}
+
+	t.FinishTime = time.Now().UTC()
+
+	t.State = task.Completed
+
+	w.Db[t.ID] = &t
+
+	log.Printf("Stopped and removed container %v for task %v\n", t.ContainerID, t.ID)
 }
